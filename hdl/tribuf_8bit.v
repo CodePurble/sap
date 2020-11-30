303a40c84ffcd4c4758735790d19277534eaa374
Ramprakash Chandrashekar <ramprakash.chandrashekar@gmail.com>
2020-12-01 00:36:11 +0530

    update: Complete docs for tribuf

--- hdl/tribuf_8bit.v
+++ hdl/tribuf_8bit.v
@@ -1,6 +1,7 @@
+// 8-bit tristate buffer
 module tribuf_8bit(in, out, enable);
 	input [7:0] in; // Input word
-	input enable;
+	input enable; // Enable (active low)
 	output tri [7:0] out; // Output word
 	assign out = enable ? 8'bzzzzzzzz : in;
 endmodule
