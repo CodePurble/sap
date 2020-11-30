8805aa8b814a158fe56665a80e0a270311e65824
Ramprakash Chandrashekar <ramprakash.chandrashekar@gmail.com>
2020-11-30 16:08:04 +0530

    update: use same file paths in all files

--- build_all.sh
+++ build_all.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
-HDL_DIR="hdl"
+HDL_DIR="../hdl"
 TEST_DIR="test"
 
-for file in $TEST_DIR/*.v; do
+cd $TEST_DIR || exit 1
+
+for file in *.v; do
 	base="${file%.*}"
 	echo -e "\033[1;33mCompiling $file\033[0m"
 	iverilog -Wall -y"$HDL_DIR" -o "$base.out" "$file"
--- run_all.sh
+++ run_all.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
-HDL_DIR="hdl"
+HDL_DIR="../hdl"
 TEST_DIR="test"
 
-for file in $TEST_DIR/*.out; do
+cd $TEST_DIR || exit 1
+
+for file in *.out; do
 	echo -e "\033[1;33mRunning $file\033[0m"
 	vvp $file
 done
