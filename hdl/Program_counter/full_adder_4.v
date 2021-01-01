9f0ce39644771f85d9896a2399e05cbc9d5eaeb5
Pratyusha R <pratyusharavishankar@gmail.com>
2021-01-01 22:59:28 +0530

    Delete full_adder_4.v

--- hdl/Program_counter/full_adder_4.v
+++
@@ -1,9 +0,0 @@
-module fulladder (  input  [3:0] a,
-                  				 b, 
-                    input  cin,  
-                    output       c,  
-                  output [3:0] sum);  
-  
-   assign sum = a^b^cin;
-   assign c = (b&cin) | (a&cin) | (a&b);
-endmodule  
