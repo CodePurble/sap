d3da2e9e79dd6967ee3167495232192f88faa0d2
Skanda Prasad <skandaprasadkv00@gmail.com>
2021-01-13 23:03:24 +0530

    wip: implement program counter

---
+++ hdl/program_counter.v
@@ -0,0 +1,17 @@
+module program_counter(
+    input cin, inc, clk, pc_out_en, clr,
+    output [3:0] out
+);
+
+wire [3:0] sum, regout;
+wire not_pc_out_en;
+
+parameter const_1 = 4'b0001;
+parameter const_0 = 1'b0;
+          
+not g1(not_pc_out_en, pc_out_en);
+adder_ripple_4bit adder(.a(regout), .b(const_1), .cin(const_0), .cout(), .sum(sum));
+reg_4bit register(.in(sum), .out(regout), .clk(clk), .low_in_en(inc), .async_reset(clr));
+tribuf_4bit buffer(.in(regout), .out(out), .low_enable(not_pc_out_en));
+
+endmodule
---
+++ test/tb_program_counter.v
@@ -0,0 +1,31 @@
+module tb_program_counter();
+    reg cin, inc, clk, pc_out_en, clr;
+    wire [3:0] out;
+
+initial
+    clk = 1'b0;
+
+always
+    #5 clk = ~clk;
+
+initial 
+    begin
+        $dumpfile("simulation/tb_program_counter.vcd");
+        $dumpvars(0, tb_program_counter);
+    end
+
+initial 
+    begin
+        #2 clr = 1'b1;
+        #10 clr = 1'b0;
+        #10 pc_out_en = 1'b1;
+        #10 pc_out_en = 1'b0;
+        #10 inc = 1'b1;
+        #300 $finish;
+    end
+
+initial 
+    begin
+        $monitor("Out : %x", out);
+    end
+endmodule
