5ff01a983f8ba3553e3fa7f13aa6c8b2eeae31e3
Ramprakash Chandrashekar <49452779+CodePurble@users.noreply.github.com>
2021-04-03 15:12:02 +0530

    refactor: use builtin primitives for tri buffers

--- hdl/tribuf_4bit.v
+++ hdl/tribuf_4bit.v
@@ -3,6 +3,10 @@ module tribuf_4bit(in, out, low_enable);
     input [3:0] in; // Input word
     input low_enable; // Enable (active low)
     output tri [3:0] out; // Output word
-    assign out = low_enable ? 4'bzzzz : in;
+
+    bufif0(out[0], in[0], low_enable);
+    bufif0(out[1], in[1], low_enable);
+    bufif0(out[2], in[2], low_enable);
+    bufif0(out[3], in[3], low_enable);
 endmodule
 
--- hdl/tribuf_8bit.v
+++ hdl/tribuf_8bit.v
@@ -3,5 +3,6 @@ module tribuf_8bit(in, out, low_enable);
     input [7:0] in; // Input word
     input low_enable; // Enable (active low)
     output tri [7:0] out; // Output word
-    assign out = low_enable ? 8'bzzzzzzzz : in;
+    tribuf_4bit b0(.in(in[3:0]), .out(out[3:0]), .low_enable(low_enable));
+    tribuf_4bit b1(.in(in[7:4]), .out(out[7:4]), .low_enable(low_enable));
 endmodule
